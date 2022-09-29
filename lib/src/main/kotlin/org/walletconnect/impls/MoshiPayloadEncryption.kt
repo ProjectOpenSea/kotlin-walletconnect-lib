ed2f2413a8b794da5bedf0666f5dfb3bffc8808c
Prashan Dharmasena <prashan.dharmasena@joinozone.com>
2022-09-29 11:45:06 -0400

    Verify HMAC when decrypting received payloads.

--- lib/src/main/kotlin/org/walletconnect/impls/MoshiPayloadEncryption.kt
+++ lib/src/main/kotlin/org/walletconnect/impls/MoshiPayloadEncryption.kt
@@ -56,20 +56,34 @@ class MoshiPayloadEncryption(moshi: Moshi) : Session.PayloadEncryption {
     override fun decrypt(encryptedPayloadJson: String, key: String): String {
         val encryptedPayload = encryptedPayloadAdapter.fromJson(encryptedPayloadJson) ?: throw IllegalArgumentException("Invalid json payload!")
 
-        // TODO verify hmac
+        val hexKey = decode(key)
+        val iv = decode(encryptedPayload.iv)
+        val encryptedData = decode(encryptedPayload.data)
+        val providedHmac = decode(encryptedPayload.hmac)
 
+        // verify hmac
+        val hmac = HMac(SHA256Digest())
+        hmac.init(KeyParameter(hexKey))
+
+        val hmacResult = ByteArray(hmac.macSize)
+        hmac.update(encryptedData, 0, encryptedData.size)
+        hmac.update(iv, 0, iv.size)
+        hmac.doFinal(hmacResult, 0)
+
+        if (!hmacResult.contentEquals(providedHmac)) throw IllegalArgumentException("HMAC does not match - expected: $hmacResult received: $providedHmac")
+
+        // decrypt payload
         val padding = PKCS7Padding()
         val aes = PaddedBufferedBlockCipher(
             CBCBlockCipher(AESEngine()),
             padding
         )
         val ivAndKey = ParametersWithIV(
-            KeyParameter(decode(key)),
-            decode(encryptedPayload.iv)
+            KeyParameter(hexKey),
+            iv
         )
         aes.init(false, ivAndKey)
 
-        val encryptedData = decode(encryptedPayload.data)
         val minSize = aes.getOutputSize(encryptedData.size)
         val outBuf = ByteArray(minSize)
         var len = aes.processBytes(encryptedData, 0, encryptedData.size, outBuf, 0)
